6928892429ff976b5d300f32b5680b3b0e0c38b4
Jeremy Thorne <jez@jezandbecks.co.uk>
2021-03-20 18:47:23 +0000

    store generic data with HammingCode
    Now acts more like a hashmap where the HammingCode is the key
    and arbitrary type T is the value
    
    also loop over different k, l values to see how execution time and
    match rate varies.
    
    The sweet spot with our 128 bit uniformly distributed Hamming Codes
    seems to be about k = 11, l = 4, which gives > 99% matches with
    sub 20ms lookup for the 10000 items. Higher k needs higher l to reach
    the same match rate and so execution time doesn't decrease. Higher k
    also uses more memory (buckets are 1 << k sized). Lower k gives too
    large buckets and so too many matches to compare against and too high
    execution times.

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,19 @@ fn hamming_distance(a:HammingCode, b: HammingCode) -> u32 {
     return (a ^ b).count_ones() as u32
 }
 
-struct HammingTable {
+struct HammingTable<T> {
     hyperplanes: Vec<HammingCode>,
-    buckets: Vec<Vec<HammingCode>>
+    buckets: Vec<Vec<(HammingCode, T)>>
 }
 
-impl HammingTable {
-    fn new(k: u32) -> HammingTable {
+impl<T:Clone> HammingTable<T> {
+    fn new(k: u32) -> HammingTable<T> {
         let mut b: Vec<u32> = (0..128).collect();
         b.shuffle(&mut rand::thread_rng());
 
         let hyperplanes: Vec<HammingCode> = 
             b[0..k as usize].iter().map(|a| 1 << a).collect();
-        let buckets = vec!(Vec::<HammingCode>::new(); 1 << k as usize);
+        let buckets = vec!(Vec::<(HammingCode, T)>::new(); 1 << k as usize);
 
         HammingTable {
             hyperplanes: hyperplanes,
@@ -40,58 +40,64 @@ impl HammingTable {
         hash
     }
 
-    fn insert(&mut self, v: HammingCode) {
-        let h = self.hash(v);
-        self.buckets[h as usize].push(v);
+    fn insert(&mut self, k: HammingCode, v:T) {
+        let h = self.hash(k);
+        self.buckets[h as usize].push((k, v));
     }
 
-    fn get(&self, v: HammingCode) -> HammingCode {
-        let h = self.hash(v);
+    fn get(&self, k: HammingCode) -> Option<&(HammingCode, T)> {
+        let h = self.hash(k);
         let mut min = u32::MAX;
-        let mut c:HammingCode = 0;
+        let mut best:Option<&(HammingCode, T)> = None;
         for n in self.buckets[h as usize].iter() {
-            let d = hamming_distance(*n, v);
+            let d = hamming_distance(n.0, k);
             if d < min {
                 min = d;
-                c = *n;
+                best = Some(n);
             }
         }
-        c
+        best
     }
 }
 
-struct HammingLSH {
-    tables: Vec<HammingTable>
+struct HammingLSH<T> {
+    tables: Vec<HammingTable<usize>>,
+    data: Vec<T>
 }
 
-impl HammingLSH {
-    fn new(k: u32, l: u32) -> HammingLSH{
-        let mut tables = Vec::<HammingTable>::new();
+impl<T:Clone> HammingLSH<T> {
+    fn new(k: u32, l: u32) -> HammingLSH<T> {
+        let mut tables = Vec::<HammingTable<usize>>::new();
         for _ in 0..l {
-            tables.push(HammingTable::new(k));
+            tables.push(HammingTable::<usize>::new(k));
         }
         HammingLSH {
-            tables: tables
+            tables: tables,
+            data: Vec::<T>::new()
         }
     }
 
-    fn insert(&mut self, v: HammingCode) {
+    fn insert(&mut self, k: HammingCode, v:T) {
+        let i = self.data.len();
+        self.data.push(v);
         for t in self.tables.iter_mut() {
-            t.insert(v);
+            t.insert(k, i);
         }
     }
 
-    fn get(&self, v: HammingCode) -> HammingCode {
+    fn get(&self, v: HammingCode) -> Option<(HammingCode, &T)> {
         let mut min = u32::MAX;
-        let mut c:HammingCode = 0;
+        let mut best:Option<(HammingCode, &T)> = None;
         for n in self.tables.iter().map(|t| t.get(v)) {
-            let d = hamming_distance(n, v);
-            if d < min {
-                min = d;
-                c = n;
+            if let Some((k, i)) = n {
+                let d = hamming_distance(*k, v);
+                if d < min {
+                    min = d;
+                    best = Some((*k, &self.data[*i]));
+                }
             }
         }
-        c
+        best
     }
 }
 
@@ -118,46 +124,54 @@ mod tests {
     }
 }
 
-fn main() {
-    println!("Hello, world!");
-    const N:usize = 10000;
-    const L:u32 = 4;
-    const K:u32 = 7; // we make a vec of length 1 << K
-    const F:u32 = 4;
-    println!("inserting {} 128 bit hamming codes into a Locality Sensitive Hash with {} tables each with {} hyperplanes",
-             N, L, K);
-    let v: Vec<HammingCode> = rand::thread_rng()
-        .sample_iter(&rand::distributions::Standard)
-        .take(N)
-        .collect();
+fn test_lsh(k:u32, l:u32, v:&Vec<HammingCode>, f:u32) {
 
-    let mut lsh = HammingLSH::new(K, L);
+    let mut lsh = HammingLSH::new(k, l);
     
     let now = SystemTime::now();
-    for i in v.iter() {
-        lsh.insert(*i)
+    for (i, j) in v.iter().enumerate() {
+        lsh.insert(*j, i);
     }
-    println!("insertion took {}ms", now.elapsed().unwrap().as_millis());
+    let insertion_time = now.elapsed().unwrap().as_millis();
 
-    println!("for each of the {} entries, perturbing {} bits and then looking in the LSH for a match",
-             N, F);
-
-    let v_p: Vec<(u128, u128)> = v.iter()
-        .map(|a| (*a, hamming_peturb(*a, F)))
+    let v_p: Vec<(usize, u128)> = v.iter()
+        .map(|a| hamming_peturb(*a, f))
+        .enumerate()
         .collect();
     
     let now = SystemTime::now();
     let mut sum = 0;
     let mut distance = 0.0;
     for (i, a) in v_p.iter() {
-        let n = lsh.get(*a);
-        if n == *i {
-            sum += 1;
-            distance += hamming_distance(n, *a) as f64;
+        if let Some(n) = lsh.get(*a) {
+            if *n.1 == *i {
+                sum += 1;
+                distance += hamming_distance(n.0, *a) as f64;
+            }
         }
     }
-    println!("lookup took {}ms", now.elapsed().unwrap().as_millis());
+    let lookup_time = now.elapsed().unwrap().as_millis();
+    let match_rate = (sum as f64 * 100.0) / (v.len() as f64);
+    let average_distance = distance / ( sum as f64);
+    println!("k {}, l {}, matched {}, avg dist {}, insertion {}ms, lookup {}ms",
+             k, l, match_rate, average_distance, insertion_time, lookup_time);
+}
 
-    println!("matched {} out of {}", sum, N);
-    println!("average distance of matches {}", distance / ( sum as f64));
+fn main() {
+    println!("Hello, world!");
+    const N:usize = 10000;
+    const F:u32 = 4;
+    let v: Vec<HammingCode> = rand::thread_rng()
+        .sample_iter(&rand::distributions::Standard)
+        .take(N)
+        .collect();
+
+    println!("inserting {} values into different sized LHS, peturbing {} bits and then performing lookup",
+             N, F);
+
+    for k in 1..16 {
+        for l in 1..8 {
+            test_lsh(k, l, &v, F);
+        }
+    }
 }
