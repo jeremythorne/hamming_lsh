82ad9b520fd0be030e14da5333a83a1e03912439
Jeremy Thorne <jez@jezandbecks.co.uk>
2021-03-20 15:38:30 +0000

    added multiple tables
    now you can have an LSH with L tables each with K hyperplanes
    
    TODO impl Ord for HammingCode so we can do iter().min() rather
    than custom loops to find minimums

--- src/main.rs
+++ src/main.rs
@@ -8,27 +8,21 @@ fn hamming_distance(a:HammingCode, b: HammingCode) -> u32 {
     return (a ^ b).count_ones() as u32
 }
 
-struct HammingLSH {
+struct HammingTable {
     hyperplanes: Vec<HammingCode>,
     buckets: Vec<Vec<HammingCode>>
 }
 
-impl HammingLSH {
-    fn new(k: u32) -> HammingLSH {
+impl HammingTable {
+    fn new(k: u32) -> HammingTable {
         let mut b: Vec<u32> = (0..128).collect();
         b.shuffle(&mut rand::thread_rng());
 
         let hyperplanes: Vec<HammingCode> = 
             b[0..k as usize].iter().map(|a| 1 << a).collect();
-        /*
-            rand::thread_rng()
-            .sample_iter(&rand::distributions::Standard)
-            .take(k as usize)
-            .collect();
-        */
         let buckets = vec!(Vec::<HammingCode>::new(); 1 << k as usize);
 
-        HammingLSH {
+        HammingTable {
             hyperplanes: hyperplanes,
             buckets: buckets
         }
@@ -46,12 +40,12 @@ impl HammingLSH {
         hash
     }
 
-    fn add(&mut self, v: HammingCode) {
+    fn insert(&mut self, v: HammingCode) {
         let h = self.hash(v);
         self.buckets[h as usize].push(v);
     }
 
-    fn approx_nn(&self, v: HammingCode) -> HammingCode {
+    fn get(&self, v: HammingCode) -> HammingCode {
         let h = self.hash(v);
         let mut min = u32::MAX;
         let mut c:HammingCode = 0;
@@ -60,6 +54,41 @@ impl HammingLSH {
             if d < min {
                 min = d;
                 c = *n;
+            }
+        }
+        c
+    }
+}
+
+struct HammingLSH {
+    tables: Vec<HammingTable>
+}
+
+impl HammingLSH {
+    fn new(k: u32, l: u32) -> HammingLSH{
+        let mut tables = Vec::<HammingTable>::new();
+        for _ in 0..l {
+            tables.push(HammingTable::new(k));
+        }
+        HammingLSH {
+            tables: tables
+        }
+    }
+
+    fn insert(&mut self, v: HammingCode) {
+        for t in self.tables.iter_mut() {
+            t.insert(v);
+        }
+    }
+
+    fn get(&self, v: HammingCode) -> HammingCode {
+        let mut min = u32::MAX;
+        let mut c:HammingCode = 0;
+        for n in self.tables.iter().map(|t| t.get(v)) {
+            let d = hamming_distance(n, v);
+            if d < min {
+                min = d;
+                c = n;
             }
         }
         c
@@ -92,20 +121,21 @@ mod tests {
 fn main() {
     println!("Hello, world!");
     const N:usize = 10000;
+    const L:u32 = 4;
     const K:u32 = 7; // we make a vec of length 1 << K
     const F:u32 = 4;
-    println!("inserting {} 128 bit hamming codes into a Locality Sensitive Hash with {} hyperplanes",
-             N, K);
+    println!("inserting {} 128 bit hamming codes into a Locality Sensitive Hash with {} tables each with {} hyperplanes",
+             N, L, K);
     let v: Vec<HammingCode> = rand::thread_rng()
         .sample_iter(&rand::distributions::Standard)
         .take(N)
         .collect();
 
-    let mut lsh = HammingLSH::new(K);
+    let mut lsh = HammingLSH::new(K, L);
     
     let now = SystemTime::now();
     for i in v.iter() {
-        lsh.add(*i)
+        lsh.insert(*i)
     }
     println!("insertion took {}ms", now.elapsed().unwrap().as_millis());
 
@@ -120,7 +150,7 @@ fn main() {
     let mut sum = 0;
     let mut distance = 0.0;
     for (i, a) in v_p.iter() {
-        let n = lsh.approx_nn(*a);
+        let n = lsh.get(*a);
         if n == *i {
             sum += 1;
             distance += hamming_distance(n, *a) as f64;
